cf42139b1d6b214723383ba0746085b865da7b77
barret917 <151187174+barret917@users.noreply.github.com>
2025-02-22 13:58:43 +0300

    Delete .history directory

--- .history/client/src/entities/Orders/hooks/index_20250220005520.js
+++
@@ -1,6 +0,0 @@
-export { useGetOrders } from './useGetOrders';
-export { apiRequestOrders } from '../api/index';
-export { useCreateOrder } from './useCreateOrder';
-export { useUpdateOrder } from './useUpdateOrder';
-export { useDeleteOrder } from './useDeleteOrder';
-export {useGetTotalByCity} from "./"
--- .history/server/src/models/associations_20250220111451.js
+++
@@ -1,6 +0,0 @@
-import Courier from './courierModel.js';
-import Order from './orderModel.js';
-
-
-Courier.hasMany(Order, { foreignKey: 'courier_id', onDelete: 'CASCADE' });
-Order.belongsTo(Courier, { foreignKey: 'courier_id' });
